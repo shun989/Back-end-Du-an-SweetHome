084a858431362a87a17d4380c81e94c373b0b3c8
hoaithuong2002 <dgth2o2@gmail.com>
2021-07-30 14:51:50 +0700

    Readme

--- Laravel-SweetHome/app/Http/Controllers/Api/AuthController.php
+++ Laravel-SweetHome/app/Http/Controllers/Api/AuthController.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use  Illuminate\Support\Facades\Auth;
-//use Illuminate\Support\Facades\Validator;
-//use Laravel\Socialite\Facades\Socialite;
-use Validator;
+use Illuminate\Support\Facades\Validator;
+use Laravel\Socialite\Facades\Socialite;
+// use Validator;
 use Exception;
 
 
