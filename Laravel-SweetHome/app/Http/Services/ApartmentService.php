6d0e713b40f5015b988699e9fbc5b1abd1a33225
aud1ence <daruma19051890@gmail.com>
2021-07-30 13:59:29 +0700

    create repo service

--- Laravel-SweetHome/app/Http/Services/ApartmentService.php
+++ Laravel-SweetHome/app/Http/Services/ApartmentService.php
@@ -4,27 +4,10 @@ namespace App\Http\Services;
 
 interface ApartmentService
 {
-<<<<<<< HEAD
+
     public function getAll();
     public function findById($id);
     public function create($request);
     public function update($request, $id);
     public function destroy($id);
-=======
-    protected ApartmentRepo $apartmentRepo;
-
-    public function __construct(ApartmentRepo  $apartmentRepository)
-    {
-        $this->apartmentRepo = $apartmentRepository;
-    }
-
-    function getById($id) {
-        return $this->apartmentRepo->getById($id);
-    }
-
-    function getAll()
-    {
-        return $this->apartmentRepo->getAll();
-    }
->>>>>>> 5feb7d2eecb7ff58fb6d14b469e80dacb07be1e3
 }
